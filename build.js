2757494af76956f536bf622a63f04ee2abab0ea0
PPz <ppzreboot@qq.com>
2026-04-08 14:46:36 +0800

    chore: prepare for publish

--- build.js
+++ build.js
@@ -3,7 +3,7 @@ import fs from 'node:fs'
 import { build } from 'esbuild'
 
 fs.rmSync('dist', { recursive: true, force: true })
-build({
+await build({
 	entryPoints: ['src/index.ts'],
 	bundle: true,
 	platform: 'browser',
